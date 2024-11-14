c33c912581f2ebe08453960a7728a6845e90732a
nicolasdani123 <155575451+nicolasdani123@users.noreply.github.com>
2024-11-13 22:28:41 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const idade = 18;
+const quantidadeItens = 3
+const promoçao = "promo2024"
+
+if(idade >= 18 && quantidadeItens >=3 && promoçao === "promo2024"){
+    console.log("voçe elegivel a promoçao")
+}
+else{
+    console.log("voçe não e elegivel")
+}
+
+const { nome:m}
